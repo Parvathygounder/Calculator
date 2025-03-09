218d186beb4c7f5c33d2a668bbd3ab792ee15232
Parvathygounder <parvathygounder@gmail.com>
2025-03-09 10:24:48 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,18 @@
+
+const screen=document.getElementById("screen")
+function display(input)
+{
+screen.value+=input;
+}
+function clea()
+{
+    screen.value="";
+}
+function result()
+{
+    screen.value=eval(screen.value);
+}
+function del()
+{
+   screen.value=screen.value.slice(0,-1);
+}
